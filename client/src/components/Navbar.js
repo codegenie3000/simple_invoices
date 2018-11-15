50e5ef207be467279bd0901cfda269109a6f49ee
Jonathan Peralez <jperalez@gmail.com>
2018-11-15 13:36:40 -0800

    Removed underline from navbar links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,18 +12,18 @@ class Navbar extends Component {
                     </div>
                     <ul className={ styles.ul }>
                         <li className={ styles.li }>
-                            <Link to="/about">About</Link>
+                            <Link className={ styles.link } to="/about">About</Link>
                         </li>
                         <li className={ styles.li }>
-                            <Link to="/login">Log In</Link>
+                            <Link className={ styles.link } to="/login">Log In</Link>
                         </li>
                     </ul>
-                    <div className={styles.dropdownButton}>
+                    <div className={ styles.dropdownButton }>
                         <i className="fa fa-bars fa-lg"></i>
                     </div>
-                    <div className={styles.sideMenu}>
-                        <Link className={styles.sideMenuLink} to="/login">Login</Link>
-                        <Link className={styles.sideMenuLink} to="/about">About</Link>
+                    <div className={ styles.sideMenu }>
+                        <Link className={ styles.sideMenuLink } to="/login">Login</Link>
+                        <Link className={ styles.sideMenuLink } to="/about">About</Link>
                     </div>
                 </div>
             </div>
