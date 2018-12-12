19b2a9d563ce38a3d4a5c54741727897828e0d12
Jonathan Peralez <jperalez@gmail.com>
2018-12-12 14:57:45 -0800

    Removed commented out code and updated comments on SignUp container

--- client/src/containers/signup/SignUp.js
+++ client/src/containers/signup/SignUp.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { reduxForm, Field } from 'redux-form';
 import * as actions from '../../actions/index';
-import globalStyles from '../../stylesheets/GlobalElements.module.css';
-import ButtonPrimary from '../../components/global/Button';
 
 import SignUpForm from '../../components/signUp/SignUpForm';
 
 // SignUp Container is used to connect the component to the redux store
 const SignUpContainer = props => {
+    // postNewUserLogin is passed from redux actions
     const { postNewUserLogin } = props;
 
     return (
