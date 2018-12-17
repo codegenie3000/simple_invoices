208e2770359ed224390bd6bfa57a59a582a17e50
Jonathan Peralez <jperalez@gmail.com>
2018-12-17 15:30:26 -0800

    Action creators now use redux-thunk instead redux-promise.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,47 +3,18 @@ import { FETCH_USER } from './types';
 import { POST_USER } from './types';
 import { FETCH_RECIPIENTS } from './types';
 
-/*export function postNewUserLogin(values) {
-    console.log(values);
-    /!*return {
-        type: FETCH_USER,
-        payload: values
-    };*!/
-    const request = axios.post('/auth/local/signup', values);
-    return {
-        type: FETCH_USER,
-        payload: request
-    }
-}*/
-
 export const postNewUser = (values) => async dispatch => {
     //TODO check if API returns the current user data once a user is signed up
-    const post = axios.post('/auth/local/signup');
+    const post = await axios.post('/auth/local/signup');
     dispatch({
         type: POST_USER,
         payload: post.data
     });
 };
 
-/*
-export function fetchRecipients() {
-    const request = axios.get('/api/recipients');
-    return {
-        type: FETCH_RECIPIENTS,
-        payload: request
-    }
-}*/
 
 export const fetchRecipients = async dispatch => {
-    // const res = axios.get('/api/recipients');
-    const res = {data: [
-            {
-                name: 'Jimmy McNulty',
-                balance: 1000.25,
-                id: 'abc123'
-            }
-        ]
-    };
+    const res = await axios.get('/api/recipients');
 
     dispatch({
         type: FETCH_RECIPIENTS,
--- client/src/actions/index.test.js
+++ client/src/actions/index.test.js
@@ -1,41 +1,66 @@
 import configureMockStore from 'redux-mock-store';
-import promise from 'redux-promise';
+// import promise from 'redux-promise';
+//TODO change to redux-thunk
 import axios from 'axios'
+import reduxThunk from 'redux-thunk';
 import MockAdapter from 'axios-mock-adapter';
 import * as actions from './index';
 import { FETCH_RECIPIENTS } from './types';
 
-const middlewares = [ promise ];
+// import initialState
+import { initialState } from '../reducers/recipientsReducer';
 
+// const middlewares = [ promise ];
+const middlewares = [ reduxThunk ];
 const mockStore = configureMockStore(middlewares);
 
 const mock = new MockAdapter(axios);
 
-describe('actions', () => {
-    it('fetches the recipients', () => {
-        mock.onGet('/api/recipients', {}).reply(200, [
+describe('initial state', () => {
+    it('tests the initial state', () => {
+        expect(initialState.isLoading).toEqual(true);
+        expect(initialState.recipients.length).toEqual(0);
+    });
+});
+
+// provide a reply to a get request to an API
+// mock the API reply via mock = new MockAdapter(axios)
+
+describe('test fetch recipients', () => {
+    let store, expectedActions, data;
+
+    beforeAll(() => {
+        data = [ {
+            name: 'Jimmy McNulty',
+            balance: 1000.25,
+            id: 'abc123'
+        } ];
+
+        mock.onGet('/api/recipients').reply(200, data);
+
+        // create mock store
+        store = mockStore(initialState);
+
+        // create an expected reply to match to
+        expectedActions = [
             {
-                ownerId: 'abc123',
-                name: 'Jimmy McNulty',
-                email: 'jimmymcnulty@thewire.com'
+                type: FETCH_RECIPIENTS, payload: [
+                    {
+                        name: 'Jimmy McNulty',
+                        balance: 1000.25,
+                        id: 'abc123'
+                    }
+                ]
             }
-        ]);
+        ];
+    });
+    it('tests that the type is correct', () => {
+        return store.dispatch(actions.fetchRecipients).then(() => {
+            expect(store.getActions()[ 0 ].type).toEqual(expectedActions[ 0 ].type);
 
-        const expectedActions = {
-            type: FETCH_RECIPIENTS,
-            payload: [ {
-                ownerId: 'abc123',
-                name: 'Jimmy McNulty',
-                email: 'jimmymcnulty@thewire.com'
-            } ]
-        };
-
-        const store = mockStore({ recipients: [] });
-
-        return store.dispatch(actions.fetchRecipients()).then(() => {
-            const actions = store.getActions();
-            expect(expectedActions.type).toEqual(actions[ 0 ].type);
-            expect(expectedActions.payload[ 0 ]).toEqual(actions[ 0 ].payload.data[ 0 ]);
         });
+    });
+    it('tests the payload', ()=> {
+        expect(store.getActions()[ 0 ].payload).toEqual(expectedActions[ 0 ].payload);
     });
 });
