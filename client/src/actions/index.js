0b6b168bf55ab48b703a3306f4d578937fb5580e
Jonathan Peralez <jperalez@gmail.com>
2018-12-20 11:12:57 -0800

    In process of creating changes to recipients reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,16 +1,26 @@
 import axios from 'axios';
-import { FETCH_USER, RECEIVE_RECIPIENTS } from './types';
-import { POST_USER } from './types';
-import { REQUEST_RECIPIENTS } from './types';
+import { POST_USER, FETCH_USER, REQUEST_RECIPIENTS, RECEIVE_RECIPIENTS } from './types';
 
-export const postNewUser = (values) => async dispatch => {
+/*export const postNewUser = (values) => async dispatch => {
     //TODO check if API returns the current user data once a user is signed up
     const post = await axios.post('/auth/local/signup');
     dispatch({
         type: POST_USER,
         payload: post.data
     });
+};*/
+
+// create a request to add new user
+// set isLoading true using a start request
+// await the server response
+// receive the user's data
+
+export const createNewUser = async dispatch => {
+    // post to server and await a response
 };
+
+
+export const postNewUser =
 
 export const requestRecipients = () => {
     return {
--- client/src/reducers/recipientsReducer.test.js
+++ client/src/reducers/recipientsReducer.test.js
@@ -1,33 +1,51 @@
 import recipientsReducer from './recipientsReducer';
-import { REQUEST_RECIPIENTS } from '../actions/types';
-import {initialState} from './recipientsReducer';
+import { REQUEST_RECIPIENTS, RECEIVE_RECIPIENTS } from '../actions/types';
+import { initialState } from './recipientsReducer';
 
 const recipientData = [
     {
-        ownerId: 'abc123',
+        id: 'recipientId123',
+        ownerId: 'ownerId123',
         name: 'Jimmy McNulty',
-        email: 'jimmymcnulty@thewire.com'
+        email: 'jimmymcnulty@thewire.com',
+        invoices: [{id: 'invoiceId123'}]
     },
     {
-        ownerId: 'xyz456',
+        id: 'recipientId456',
+        ownerId: 'ownerId456',
         name: 'Omar Little',
-        email: 'omarlittle@thewire.com'
+        email: 'omarlittle@thewire.com',
+        invoices:[{id: 'invoiceId456'}]
     }
 ];
 
-describe('recipients reducer', () => {
+describe('initial state', () => {
     it('should return the initial state', () => {
         expect(recipientsReducer(undefined, {})).toEqual(initialState);
     });
+});
+
+describe('REQUEST_RECIPIENTS', () => {
+    it('should return the correct data', () => {
+        expect(recipientsReducer(undefined, {
+            type: REQUEST_RECIPIENTS
+        })).toEqual({
+            recipients: [],
+            isLoading: true
+        });
+    });
+});
+
+describe('RECEIVE_RECIPIENTS reducer', () => {
     it('should handle REQUEST_RECIPIENTS', () => {
         expect(recipientsReducer(
             undefined, {
-                type: REQUEST_RECIPIENTS,
+                type: RECEIVE_RECIPIENTS,
                 payload: recipientData
             })).toEqual(
             {
                 recipients: recipientData,
-                isLoading: true
+                isLoading: false
             });
     });
 });
